5b664074fcc0ed7924df5e8b1c775cc587d65799
Knisterpapier <jsurebndu@gmx.de>
2021-02-26 23:42:05 +0100

    playing with pyo lib

---
+++ dsp.py
@@ -0,0 +1,35 @@
+import numpy
+from pyo import *
+import time
+
+
+if __name__ == '__main__':
+
+ 
+
+    s = Server().boot()
+    s.start()
+
+    a = Input(chnl=0)
+    chorus = Chorus(a, depth=0.7, feedback=0.9, bal=0.5).out()
+
+
+    s.gui(locals())
+    s.stop()
+
+    # s = Server().boot()
+    # s.start()
+    
+    # f = Adsr(attack=.01, decay=.2, sustain=.5, release=.1, dur=4, mul=.5)
+    # a = Sine(mul=f).out()
+
+    # f.play()
+    # time.sleep(5)
+    
+    # f = Adsr(attack=.10, decay=.8, sustain=.2, release=.5, dur=4, mul=.5)
+    # a = Sine(mul=f).out()
+    # a.freq = 500
+    # f.play()
+
+    # s.gui(locals())
+    # s.stop()
